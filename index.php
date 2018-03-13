ca03fcb1a58d52e63df326d515f10684cec767ed
Josh Lieshout <machmatic@gmail.com>
2018-03-13 15:57:06 +1300

    Removed unused scripts

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$scriptList = array("js/jquery-3.3.1.min.js", "https://www.google.com/recaptcha/api.js");
+$scriptList = array();
 include("php/header.php");
 include("html/content.html");
 include("php/footer.php");
