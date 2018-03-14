463a9661abe7f6e8d07756a5ed11609515cd6c7a
Josh Lieshout <machmatic@gmail.com>
2018-03-14 16:59:47 +1300

    Further improved compatibility for phones

--- php/header.php
+++ php/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name=viewport content='device-width'>
+    <meta name=viewport content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Cache-Control" content="max-age=3600" />
     <title>Joshrl.me - Online Portfolio</title>
     <link rel="stylesheet" href="style.css">
